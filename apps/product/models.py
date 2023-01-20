abac8b8067a641eaa5d5d70c31c8042b675d57c6
Denis Padua <denisdepadua@outlook.com>
2023-01-20 16:43:49 -0300

    feat: price validation

--- apps/product/models.py
+++ apps/product/models.py
@@ -2,6 +2,8 @@ import uuid
 from django.db import models
 
 from apps.category.models import CategoryModel
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # Create your models here.
 class ProductModel(models.Model):
@@ -9,6 +11,6 @@ class ProductModel(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     image = models.ImageField(upload_to='images/')
     product_name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits = 8,decimal_places=2)
+    price = models.DecimalField(max_digits = 8,decimal_places=2, validators=[MinValueValidator(Decimal('0.00'))])
     description = models.CharField(max_length=300)
     category = models.ForeignKey(CategoryModel, on_delete=models.DO_NOTHING)
