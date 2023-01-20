6a0a88ed2a56fde49790643b631dee98f91e1e4a
Denis Padua <denisdepadua@outlook.com>
2023-01-20 11:56:05 -0300

    feat: create view list and template for product

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from apps.product.models import ProductModel
 
 # Create your views here.
+class ProductListView(ListView):
+    model = ProductModel
