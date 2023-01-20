40e8b1ae2785040cd77ee03d9454dbc80a18a3b4
Denis Padua <denisdepadua@outlook.com>
2023-01-20 16:24:19 -0300

    create a alias for product list view

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.product.views import ProductCreateView, ProductListView
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='product_list'),
     path('create/', ProductCreateView.as_view())
 ]
